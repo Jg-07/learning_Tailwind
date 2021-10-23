e90e100ef8b6018e7e7a2f60a9bb4b665c8a6fca
Jg-07 <jaiastrobaskar@gmail.com>
2021-10-23 22:51:56 +0530

    commit

--- learning_TailWind/src/SeverNavBar.jsx
+++ learning_TailWind/src/SeverNavBar.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdKeyboardArrowDown, MdKeyboardArrowRight } from "react-icons/md";
+import { FaPlus } from "react-icons/fa";
+
+const topics = ['tailwind-css','react','python']
+const questions = ['react-dom','jit']
+const random = ['memes','quiz']
+
 
 const SeverNavBar = () => {
     return(
-        <div className="fixed z-[-1] top-0 left-16 w-60 h-screen
-         bg-gray-800 text-white font-bold flex flex-col
-         ">
-             <NavHeader/>
+        <div className="sever-nav-bar">
+             <span className="sever-head">Channels</span>
+             <NavContent headText={"Topics"} selections={topics}/>
+             <NavContent headText={"Questions"} selections={questions}/>
+             <NavContent headText={"Random"} selections={random}/>
         </div>
     );
 };
 
-const NavHeader = () => {
+const NavContent = ({headText,selections}) => {
+    const [expanded,setExpanded] = useState(true);
+
     return(
-        <div className="sever-head">
-            Channels
+        <div className="nav-content">
+            <div onClick ={() => setExpanded(!expanded)} className=" flex">
+                <MdKeyboardArrowRight size='28' />
+                <span className="head-text">
+                {headText}
+                </span>
+            </div>
+            <span className="pr-4"><FaPlus size="14"/></span>
         </div>
     );
 }
