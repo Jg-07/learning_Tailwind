7f5b2334b27f3f3fd7dff01f5dd48901e62a9c8e
Jg-07 <jaiastrobaskar@gmail.com>
2021-10-23 13:01:49 +0530

    commit

--- learning_TailWind/tailwind.config.js
+++ learning_TailWind/tailwind.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      color:{
-        primary : '#202225',
-        secondary: '#5865f2',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
